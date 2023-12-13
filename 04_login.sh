f63b8ebb62f2ae940698cb351a6a5ca037eda139
unknown <bsatheesh779@gmail.com>
2023-12-13 21:24:24 +0530

    second file added

---
+++ 04_login.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "please enter your username"
+read -s USERNAME
+echo "please enter your password"
+read -s PASSWORD
+echo "you username is ${USERNAME}"
+echo "you username is ${PASSWORD}"
