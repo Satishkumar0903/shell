3e7e3fd612c0386fd6fd11c3eb312554b7d9f148
unknown <bsatheesh779@gmail.com>
2023-12-13 21:06:50 +0530

    second file added

--- 02_variables.sh
+++ 02_variables.sh
@@ -3,6 +3,6 @@ person1=ramesh
 person2=suresh
 
 echo "$person1: Hello $person2, Good Morning"
-echo "$person2: Hi $PERSON1, very Good Morning"
+echo "$person2: Hi $person1, very Good Morning"
 echo "$person1: How are you $person2?"
 echo "$person2: I am good $person1, How are you"
