01fe7f1c2c548f095b146eaa691e003de9ab8a1f
unknown <bsatheesh779@gmail.com>
2023-12-14 22:34:58 +0530

    practice

--- 05_installing.sh
+++ 05_installing.sh
@@ -2,7 +2,15 @@
 TIME=$(date +%F-%H-%M-%S)
 LOG="/tmp/$0-$TIME.log"
 echo "Script started executing at $TIME"
-echo "checking root user are not"
+echo "checking root user are not.....?"
+validate(){
+if [ $1 -ne 0 ]
+then
+echo "Error:: $2 install failue occurs"
+else
+echo "Mysql installed successfully installed....!"
+fi
+}
 ID=$(id -u)
 if [ $ID -ne 0 ] &>> $LOG
 then
@@ -11,9 +19,7 @@ else
 echo "you are a root user"
 fi
 yum install mysql -y &>> $LOG
-if [ $? -ne 0 ]
-then
-echo "Error:: Mysql install failue occurs"
-else
-echo "Mysql installed successfully installed"
-fi
+validate $? "mysql"
+yum install git -y
+validate $? "git"
+
