dd22705bd5d3e62a7e635893597f0a7e92dcdf99
unknown <bsatheesh779@gmail.com>
2023-12-14 22:56:06 +0530

    practice

--- 05_installing.sh
+++ 05_installing.sh
@@ -1,25 +1,30 @@
 #!/bin/bash
 TIME=$(date +%F-%H-%M-%S)
 LOG="/tmp/$0-$TIME.log"
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
 echo "Script started executing at $TIME"
 echo "checking root user are not.....?"
 validate(){
 if [ $1 -ne 0 ]
 then
-echo "Error:: $2 install failue occurs"
+echo -e "Error:: $2 install $R failue occurs$N"
+exit 1
 else
-echo "$2 installed successfully ....!"
+echo "$2 installed $G successfully ....!$N"
 fi
 }
 ID=$(id -u)
 if [ $ID -ne 0 ] &>> $LOG
 then
-echo "you are not a root user"
+echo "you are not a $R root user $N"
 else
-echo "you are a root user"
+echo "you are a $G root user"
 fi
 yum installlllll mysql -y &>> $LOG
 validate $? "mysql"
-yum installlllllll git -y
+yum installlllllll git -y &>> $LOG
 validate $? "git"
+
 
